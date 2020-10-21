80bb38f374024b28f256133b9f54b57baa3f895b
DerekCHL <43765355+DrkDL@users.noreply.github.com>
2020-10-21 13:35:39 -0400

    RollABall Project

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // distance bewteen camera transform posoition and the player's
+        offset = transform.position - player.transform.position;
+    }
+
+    // LateUpdate is called once per frame
+    void LateUpdate() // used for follow cameras, procedural animation and gathering last known states
+    {
+        // in this case before the system updates it knows the player has moved and then updates
+
+        // make the camera follow with the player
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // namesapce for UI
+
+// Difference bewteen awake and start funciton:
+//-On a script that is disabled: Nothing is called(No Awake or Start functions called).
+//-On a enabled script: Awake is called first when an object 'carrying' the script is first activated(enabled/initialized), 
+// Start is then called when the game/application proceeds.
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float moveSpeed = 10f;
+    private int count; // number of pickups collected
+    public Text countText;
+    public Text winText;
+
+    void Start() // is called when script
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        setCountText();
+        winText.text = "";
+    }
+    void Update() // is called before rendering a frame, where game code will go
+    {
+        
+    }
+
+    void FixedUpdate() // is called before performing any physics calculations, where physics code will go
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
+
+        rb.AddForce(movement * moveSpeed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Pick Up")
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            setCountText();
+        }
+    }
+
+    void setCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winText.text = "You Win!";
+        }
+    }
+}
